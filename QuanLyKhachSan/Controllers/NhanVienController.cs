34e3e8e210da60d8df2bd50a2d83ad78f19ea069
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-11-11 10:48:55 +0700

    nhanvien

---
+++ QuanLyKhachSan/Controllers/NhanVienController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyKhachSan.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        public IActionResult TrangChuNhanVien()
+        {
+            return View();
+        }
+    }
+}
