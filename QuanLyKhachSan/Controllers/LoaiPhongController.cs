8f71da9cecce5fc10d1641e6096766731998fe99
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-11-18 15:45:24 +0700

    xong loai phong

--- QuanLyKhachSan/Controllers/LoaiPhongController.cs
+++ QuanLyKhachSan/Controllers/LoaiPhongController.cs
@@ -16,5 +16,47 @@ namespace QuanLyKhachSan.Controllers
             var listLoaiPhong = _db.LoaiPhong.ToList();
             return View(listLoaiPhong);
         }
+        public IActionResult XoaLoaiPhong(string id)
+        {
+            var qr_MaLoaiPhong = _db.LoaiPhong.Find(id);
+            if (qr_MaLoaiPhong != null)
+            {
+                _db.LoaiPhong.Remove(qr_MaLoaiPhong);
+                _db.SaveChanges();
+
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("TrangChuLoaiPhong", "LoaiPhong");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemLoaiPhong(LoaiPhong loaiphong)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LoaiPhong.Add(loaiphong);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("TrangChuLoaiPhong", "LoaiPhong");
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaLoaiPhong(LoaiPhong loaiphong)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LoaiPhong.Update(loaiphong);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("TrangChuLoaiPhong", "LoaiPhong");
+
+        }
     }
 }
---
+++ QuanLyKhachSan/wwwroot/jsLoaiPhong/LoaiPhong.js
@@ -0,0 +1,90 @@
+﻿var btn_them = document.getElementById('btn_themLoaiPhong');
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    btn_them.style.display = 'none';
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    btn_them.style.display = 'block';
+}
+
+// Popup for editing room types
+function OpenFormEdit() {
+    document.getElementById('myFormEdit').style.display = "block";
+}
+
+function CloseFormEdit() {
+    document.getElementById('myFormEdit').style.display = "none";
+}
+
+// Turn off the edit popup if both are open
+document.getElementById('btn_themLoaiPhong').addEventListener('click', hidePopUpEditLoaiPhong);
+
+function hidePopUpEditLoaiPhong() {
+    document.getElementById('myFormEdit').style.display = 'none';
+}
+
+// Event listener for the "Sửa" button in the dropdown menu
+document.getElementById('btn_Sua').addEventListener('click', hidePopUpAddLoaiPhong);
+
+function hidePopUpAddLoaiPhong() {
+    document.getElementById('myForm').style.display = 'none';
+    btn_them.style.display = 'block';
+}
+var getValue = document.querySelector('#table_detail');
+
+getValue.addEventListener('click', function (e) {
+    const cell = e.target.closest('td');
+    if (!cell) { return; }
+    const row = cell.parentElement;
+    var maLoaiPhong = row.dataset.maLoaiPhong;
+    var tenLoaiPhong = row.dataset.tenLoaiPhong;
+    var giaTheoGio = row.dataset.giaTheoGio;
+    var giaTheoNgay = row.dataset.giaTheoNgay;
+    var phuThuTraMuon = row.dataset.phuThuTraMuon;
+    var soLuongNguoiLon = row.dataset.soLuongNguoiLon;
+    var soLuongTreEm = row.dataset.soLuongTreEm;
+    console.log(giaTheoNgay);
+    document.querySelector(".maLPEdit").value = maLoaiPhong;
+    document.querySelector(".tenLPEdit").value = tenLoaiPhong;
+    document.querySelector(".GiaTheoGioEdit").value = giaTheoGio;
+    document.querySelector(".GiaTheoNgayEdit").value = giaTheoNgay;
+    document.querySelector(".PhuThuEdit").value = phuThuTraMuon;
+    document.querySelector(".SoNguoiLonEdit").value = soLuongNguoiLon;
+    document.querySelector(".SoTreEmEdit").value = soLuongTreEm;
+
+
+});
+// Event listener for the header checkbox to tick/untick all checkboxes
+document.getElementById('thead_checkbox').addEventListener('click', TickedAllCheckBox);
+
+function TickedAllCheckBox() {
+    var type_checkbox = document.getElementById('thead_checkbox').checked;
+    var elements_checkbox_content = document.querySelectorAll('.tbody_checkbox');
+
+    elements_checkbox_content.forEach(function (checkbox) {
+        checkbox.checked = type_checkbox;
+        KiemTraCheckBox();
+    });
+}
+
+// Check the number of selected checkboxes for room types
+function KiemTraCheckBox() {
+    var x = document.querySelectorAll('input[name="checkBox_Name"]:checked');
+    var btn = document.querySelectorAll(".check_checkbox");
+
+    for (const btn_element of btn) {
+        if (x.length > 1) {
+            btn_element.classList.add('hide_edit');
+        } else {
+            btn_element.classList.remove('hide_edit');
+        }
+    }
+}
+
+// Additional UI adjustments on page load
+window.addEventListener('load', function (event) {
+    // Perform any additional UI adjustments here
+});
