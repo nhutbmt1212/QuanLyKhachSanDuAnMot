b2e64c21afd72fc10c5a266aa8f8bdcaa4b05914
nphoang665 <phihoang665@gmail.com>
2023-11-18 15:52:01 +0700

    #GiaoDienLayout KhachHang

---
+++ QuanLyKhachSan/Controllers/KhachHangController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyKhachSan.Models;
+
+namespace QuanLyKhachSan.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public KhachHangController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult DanhSachKhachHang()
+        {
+            var listKhachHang = _db.KhachHang.ToList();
+            return View(listKhachHang);
+        }
+    }
+}
