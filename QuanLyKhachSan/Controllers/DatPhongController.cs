93dbb96a65520fb39aaf41696585a437b77dd711
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-11-20 08:44:42 +0700

    dp

--- QuanLyKhachSan/Controllers/DatPhongController.cs
+++ QuanLyKhachSan/Controllers/DatPhongController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyKhachSan.Models;
 
 namespace QuanLyKhachSan.Controllers
 {
     public class DatPhongController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public DatPhongController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var qr_Phong= _db.Phong.ToList();
+           
+            return View(qr_Phong);
         }
     }
 }
