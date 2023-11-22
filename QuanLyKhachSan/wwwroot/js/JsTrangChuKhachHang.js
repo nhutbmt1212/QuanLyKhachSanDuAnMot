074e1550adc276a53c8b7820b4a940e958438f81
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-11-22 11:09:17 +0700

    trang chu khach hang dat phong

---
+++ QuanLyKhachSan/wwwroot/js/JsTrangChuKhachHang.js
@@ -0,0 +1,26 @@
+﻿
+    $(document).ready(function () {
+        $('.room-type').each(function () {
+            var maPhong = $(this).attr('id');
+            fetchRoomType(maPhong, this);
+        });
+        });
+
+    function fetchRoomType(maPhong, element) {
+        $.ajax({
+            url: '/TrangChuKhachHang/LayLoaiPhong',
+            type: 'GET',
+            data: { maPhong: maPhong },
+            success: function (data) {
+                $(".loaiPhong").text(data.tenLoaiPhong);
+                $("#loaiPhong-" + maPhong).text("Loại phòng: " + data.tenLoaiPhong);
+
+                $("#price-" + maPhong).text("Giá theo giờ: " + data.giaTheoGio + "  |  " + "Giá theo ngày: " + data.giaPhongTheoNgay);
+                $("#soLuongNguoi-" + maPhong).text("Số lượng người lớn: " + data.soLuongNguoiLon + "  |  " + "Số lượng trẻ em: " + data.soLuongTreEm);
+                $("#phuThuTraMuon-" + maPhong).text("Phụ thu trả muộn: " + data.phuThuTraMuon + "/1 ngày");
+
+
+                console.log(data.tenLoaiPhong);
+            }
+        });
+ }
