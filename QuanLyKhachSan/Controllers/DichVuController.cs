5de154251e524fe6dbfdc91cce9032a40107452a
thanh1709 <thanhvtp2004@gmail.com>
2023-12-06 18:52:28 +0700

    xong hết import export fix lỗi trùng mã hoặc để trống

--- QuanLyKhachSan/Controllers/DichVuController.cs
+++ QuanLyKhachSan/Controllers/DichVuController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using Org.BouncyCastle.Asn1.Cms;
 using QuanLyKhachSan.Models;
+using System.Data;
 
 namespace QuanLyKhachSan.Controllers
 {
@@ -91,5 +95,179 @@ namespace QuanLyKhachSan.Controllers
             return RedirectToAction("DanhSachDichVu", "DichVu");
 
         }
+        public ActionResult ExportExcel()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("DichVu");
+
+                var dichvu = _db.DichVu.ToList();
+
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+                dataTable.Columns.Add("Mã Dịch Vụ");
+                dataTable.Columns.Add("Tên Dịch Vụ");
+                dataTable.Columns.Add("Đơn Vị Tính");
+                dataTable.Columns.Add("Giá Tiền");
+                dataTable.Columns.Add("Tình Trạng");
+                dataTable.Columns.Add("Giờ Bắt Đầu Dịch Vụ");
+                dataTable.Columns.Add("Giờ Kết Thúc Dịch Vụ");
+
+                foreach (var dv in dichvu)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    dataRow[0] = dv.MaDichVu;
+                    dataRow[1] = dv.TenDichVu;
+                    dataRow[2] = dv.DonViTinh;
+                    dataRow[3] = dv.GiaTien;
+                    dataRow[4] = dv.TinhTrang;
+                    dataRow[5] = dv.GioBatDauDichVu;
+                    dataRow[6] = dv.GioKetThucDichVu;
+                    dataTable.Rows.Add(dataRow);
+                }
+
+                ws.Cells["A1"].LoadFromDataTable(dataTable, true);
+
+                var stream = new MemoryStream(pck.GetAsByteArray());
+
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DichVu.xlsx");
+            }
+        }
+
+        public async Task<IActionResult> Import(IFormFile formFile)
+        {
+            if (formFile == null || formFile.Length <= 0)
+            {
+                return BadRequest("Chọn một file để nhập dữ liệu.");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                return BadRequest("Chỉ hỗ trợ file .xlsx");
+            }
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var stream = new MemoryStream())
+            {
+                await formFile.CopyToAsync(stream);
+
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0]; // Sử dụng sheet đầu tiên
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    List<int> errorRows = new List<int>();
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        // Kiểm tra dòng trống
+                        if (worksheet.Cells[row, 1].Value == null || worksheet.Cells[row, 2].Value == null || worksheet.Cells[row, 3].Value == null || worksheet.Cells[row, 4].Value == null || worksheet.Cells[row, 5].Value == null || worksheet.Cells[row, 6].Value == null || worksheet.Cells[row, 7].Value == null)
+                        {
+                            errorRows.Add(row);
+                            continue;
+                        }
+
+                        var MaDichVu = worksheet.Cells[row, 1].Value.ToString().Trim();
+                        var TenDichVu = worksheet.Cells[row, 2].Value.ToString().Trim();
+                        var DonViTinh = worksheet.Cells[row, 3].Value.ToString().Trim();
+                        int GiaTien;
+                        int.TryParse(worksheet.Cells[row, 4].Value.ToString().Trim(), out GiaTien); 
+                        var TinhTrang = worksheet.Cells[row, 5].Value.ToString().Trim();
+                        TimeSpan GioBatDauDichVu;
+                        TimeSpan.TryParse(worksheet.Cells[row, 6].Value.ToString().Trim(), out GioBatDauDichVu);
+                        TimeSpan GioKetThucDichVu;
+                        TimeSpan.TryParse(worksheet.Cells[row, 7].Value.ToString().Trim(), out GioKetThucDichVu);
+
+                        var dichVu = _db.DichVu.FirstOrDefault(dv => dv.MaDichVu == MaDichVu);
+                        if (dichVu != null)
+                        {
+                            // Cập nhật dịch vụ
+                            dichVu.TenDichVu = TenDichVu;
+                            dichVu.DonViTinh = DonViTinh;
+                            dichVu.GiaTien = GiaTien;
+                            dichVu.TinhTrang = TinhTrang;
+                            dichVu.GioBatDauDichVu = GioBatDauDichVu;
+                            dichVu.GioKetThucDichVu = GioKetThucDichVu;
+                        }
+                        else
+                        {
+                            // Thêm dịch vụ mới
+                            var list = new DichVu
+                            {
+                                MaDichVu = MaDichVu,
+                                TenDichVu = TenDichVu,
+                                DonViTinh = DonViTinh,
+                                GiaTien = GiaTien,
+                                TinhTrang = TinhTrang,
+                                GioBatDauDichVu = GioBatDauDichVu,
+                                GioKetThucDichVu = GioKetThucDichVu
+                            };
+                            _db.DichVu.Add(list);
+                        }
+                        _db.SaveChanges();
+                    }
+
+                    if (errorRows.Count > 0)
+                    {
+                        return BadRequest($"Có lỗi ở các dòng: {string.Join(", ", errorRows)}. Vui lòng kiểm tra lại file Excel của bạn.");
+                    }
+                }
+                return RedirectToAction("DanhSachDichVu", "DichVu");
+            }
+        }
+        public async Task<IActionResult> ExportPDF()
+        {
+            Document pdfDoc = new Document();
+            MemoryStream memoryStream = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
+
+            var dichvu = _db.DichVu.ToList();
+
+            pdfDoc.Open();
+            float cm = 20;
+            float points = cm * 72 / 2.54f;
+            float pointsForColumn = points * 1.25f;
+            PdfPTable table = new PdfPTable(7);
+            table.SetTotalWidth(new float[] { pointsForColumn, pointsForColumn, pointsForColumn, pointsForColumn, pointsForColumn, pointsForColumn, pointsForColumn });
+
+            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+
+            Font headerFont = FontFactory.GetFont("Arial", 5, Font.BOLD, BaseColor.WHITE);
+            BaseColor headerBackgroundColor = new BaseColor(0, 119, 119);
+
+            string[] headers = { "Mã Dịch Vụ", "Tên Dịch Vụ", "Đơn Vị Tính", "Giá Tiền", "Tình Trạng", "Giờ Bắt Đầu Dịch Vụ", "Giờ Kết Thúc Dịch Vụ" };
+
+            foreach (var header in headers)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(header, headerFont));
+                cell.BackgroundColor = headerBackgroundColor;
+                table.AddCell(cell);
+            }
+
+            Font contentFont = FontFactory.GetFont("Arial", 5, Font.NORMAL, BaseColor.BLACK);
+
+            foreach (var dv in dichvu)
+            {
+                table.AddCell(new Phrase(dv.MaDichVu.ToString(), contentFont));
+                table.AddCell(new Phrase(dv.TenDichVu.ToString(), contentFont));
+                table.AddCell(new Phrase(dv.DonViTinh.ToString(), contentFont));
+                table.AddCell(new Phrase(dv.GiaTien.ToString(), contentFont));
+                table.AddCell(new Phrase(dv.TinhTrang.ToString(), contentFont));
+                table.AddCell(new Phrase(dv.GioBatDauDichVu.ToString(), contentFont));
+                table.AddCell(new Phrase(dv.GioKetThucDichVu.ToString(), contentFont));
+            }
+
+            pdfDoc.Add(table);
+
+            pdfDoc.Close();
+
+            byte[] bytes = memoryStream.ToArray();
+            memoryStream.Close();
+
+            return File(bytes, "application/pdf", "DichVu.pdf");
+        }
+
+
     }
 }
--- QuanLyKhachSan/Controllers/VatTuController.cs
+++ QuanLyKhachSan/Controllers/VatTuController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using QuanLyKhachSan.Models;
 using System;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace QuanLyKhachSan.Controllers
@@ -85,5 +89,167 @@ namespace QuanLyKhachSan.Controllers
             }
             return RedirectToAction("DanhSachVatTu", "VatTu");
         }
+        public ActionResult ExportExcel()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("VatTu");
+
+                var vattu = _db.VatTu.ToList();
+
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+                dataTable.Columns.Add("Mã Vật Tư");
+                dataTable.Columns.Add("Tên Vật Tư");
+                dataTable.Columns.Add("Tình Trạng Vật Tư");
+                dataTable.Columns.Add("Ngày Thêm");
+                dataTable.Columns.Add("Nhà Sản Xuất");
+
+                foreach (var vt in vattu)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    dataRow[0] = vt.MaVatTu;
+                    dataRow[1] = vt.TenVatTu;
+                    dataRow[2] = vt.TinhTrangVatTu;
+                    dataRow[3] = vt.NgayThem.ToShortDateString();
+                    dataRow[4] = vt.NhaSanXuat;
+                    dataTable.Rows.Add(dataRow);
+                }
+
+                ws.Cells["A1"].LoadFromDataTable(dataTable, true);
+
+                var stream = new MemoryStream(pck.GetAsByteArray());
+
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "VatTu.xlsx");
+            }
+        }
+        public async Task<IActionResult> Import(IFormFile formFile)
+        {
+            if (formFile == null || formFile.Length <= 0)
+            {
+                return BadRequest("Chọn một file để nhập dữ liệu.");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                return BadRequest("Chỉ hỗ trợ file .xlsx");
+            }
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var stream = new MemoryStream())
+            {
+                await formFile.CopyToAsync(stream);
+
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0]; // Sử dụng sheet đầu tiên
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    List<int> errorRows = new List<int>();
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        // Kiểm tra dòng trống
+                        if (worksheet.Cells[row, 1].Value == null || worksheet.Cells[row, 2].Value == null || worksheet.Cells[row, 3].Value == null || worksheet.Cells[row, 4].Value == null || worksheet.Cells[row, 5].Value == null)
+                        {
+                            errorRows.Add(row);
+                            continue;
+                        }
+
+                        var MaVatTu = worksheet.Cells[row, 1].Value.ToString().Trim();
+                        var TenVatTu = worksheet.Cells[row, 2].Value.ToString().Trim();
+                        var TinhTrangVatTu = worksheet.Cells[row, 3].Value.ToString().Trim();
+                        DateTime ngayThem;
+                        DateTime.TryParse(worksheet.Cells[row, 4].Value.ToString().Trim(), out ngayThem);
+                        var NhaSanXuat = worksheet.Cells[row, 5].Value.ToString().Trim();
+
+                        var vatTu = _db.VatTu.FirstOrDefault(vt => vt.MaVatTu == MaVatTu);
+                        if (vatTu != null)
+                        {
+                            // Cập nhật vật tư
+                            vatTu.TenVatTu = TenVatTu;
+                            vatTu.TinhTrangVatTu = TinhTrangVatTu;
+                            vatTu.NgayThem = ngayThem;
+                            vatTu.NhaSanXuat = NhaSanXuat;
+                        }
+                        else
+                        {
+                            // Thêm vật tư mới
+                            var list = new VatTu
+                            {
+                                MaVatTu = MaVatTu,
+                                TenVatTu = TenVatTu,
+                                TinhTrangVatTu = TinhTrangVatTu,
+                                NgayThem = ngayThem,
+                                NhaSanXuat = NhaSanXuat
+                            };
+                            _db.VatTu.Add(list);
+                        }
+                        _db.SaveChanges();
+                    }
+
+                    if (errorRows.Count > 0)
+                    {
+                        return BadRequest($"Có lỗi ở các dòng: {string.Join(", ", errorRows)}. Vui lòng kiểm tra lại file Excel của bạn.");
+                    }
+                }
+                return RedirectToAction("DanhSachVatTu", "VatTu");
+            }
+        }
+
+
+        public async Task<IActionResult> ExportPDF()
+        {
+            Document pdfDoc = new Document();
+            MemoryStream memoryStream = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
+
+            var vattu = _db.VatTu.ToList();
+
+            pdfDoc.Open();
+            float cm = 20;
+            float points = cm * 72 / 2.54f;
+            float pointsForColumn = points * 1.5f;
+            PdfPTable table = new PdfPTable(5);
+            table.SetTotalWidth(new float[] { pointsForColumn, pointsForColumn, pointsForColumn, pointsForColumn, pointsForColumn });
+
+            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+
+            Font headerFont = FontFactory.GetFont("Arial", 10, Font.BOLD, BaseColor.WHITE);
+            BaseColor headerBackgroundColor = new BaseColor(0, 119, 119);
+
+            string[] headers = { "Mã Vật Tư", "Tên Vật Tư", "Tình Trạng Vật Tư", "Ngày Thêm", "Nhà Sản Xuất" };
+
+            foreach (var header in headers)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(header, headerFont));
+                cell.BackgroundColor = headerBackgroundColor;
+                table.AddCell(cell);
+            }
+
+            Font contentFont = FontFactory.GetFont("Arial", 10, Font.NORMAL, BaseColor.BLACK);
+
+            foreach (var vt in vattu)
+            {
+                table.AddCell(new Phrase(vt.MaVatTu.ToString(), contentFont));
+                table.AddCell(new Phrase(vt.TenVatTu.ToString(), contentFont));
+                table.AddCell(new Phrase(vt.TinhTrangVatTu.ToString(), contentFont));
+                table.AddCell(new Phrase(vt.NgayThem.ToShortDateString().ToString(), contentFont));
+                table.AddCell(new Phrase(vt.NhaSanXuat.ToString(), contentFont));
+            }
+
+            pdfDoc.Add(table);
+
+            pdfDoc.Close();
+
+            byte[] bytes = memoryStream.ToArray();
+            memoryStream.Close();
+
+            return File(bytes, "application/pdf", "VatTu.pdf");
+        }
+
+
+
     }
 }
