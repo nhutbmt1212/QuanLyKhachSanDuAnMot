7791c9f071dafff5ff1f79139c133b9e5b021464
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-12-08 12:33:38 +0700

    datphong

--- QuanLyKhachSan/Controllers/DatPhongController.cs
+++ QuanLyKhachSan/Controllers/DatPhongController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using QuanLyKhachSan.Models;
+using System;
+using System.Security.Claims;
 
 namespace QuanLyKhachSan.Controllers
 {
@@ -37,6 +39,93 @@ namespace QuanLyKhachSan.Controllers
         {
             var qr_ListDichVu = _db.DichVu.ToList();
             return Json(qr_ListDichVu);
+        }
+        [HttpPost]
+        public IActionResult TinhTienDichVu(List<string> arrMaDichVu, List<string> arrSoLuongDichVu)
+        {
+            decimal tongTien = 0;
+            for (int i = 0; i < arrMaDichVu.Count; i++)
+            {
+                var maDichVu = arrMaDichVu[i];
+                var soLuong = int.Parse(arrSoLuongDichVu[i]);
+
+                // Giả sử bạn có một bảng DịchVụ trong cơ sở dữ liệu với các cột MaDichVu, GiaTien
+                var dichVu = _db.DichVu.SingleOrDefault(dv => dv.MaDichVu == maDichVu);
+                if (dichVu != null)
+                {
+                    tongTien += dichVu.GiaTien * soLuong;
+                }
+            }
+            return Json(tongTien);
+        }
+        [HttpPost]
+        public IActionResult DatPhongNhanh(List<string> arrMaDichVu, List<int> arrSoLuongDichVu, string maKhachHang, string hinhThuc, DateTime ngayNhan, DateTime ngayTra, int tongNguoiLon, int tongTreEm, int tongCong, int soTienTraTruoc,string maPhong)
+        {
+            Random random = new Random();
+
+            // mã random mã đặt phòng
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string randomCharacterLastMaDatPhong = new string(Enumerable.Repeat(chars, 4)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+            string MaDatPhong = "DP" + randomCharacterLastMaDatPhong;
+            //mã nhân viên đặt phòng
+            var MaNhanVienDatPhong = User.FindFirst(ClaimTypes.Surname)?.Value;
+            var DatPhong = new DatPhong
+            {
+                MaDatPhong = MaDatPhong,
+                MaKhachHang = maKhachHang,
+                MaPhong = maPhong,
+                NgayNhan = ngayNhan,
+                NgayTra = ngayTra,
+                SoLuongNguoiLon = tongNguoiLon,
+                SoLuongTreEm = tongTreEm,
+                HinhThucDatPhong = hinhThuc,
+                TongTienPhong = tongCong,
+                MaNhanVien = MaNhanVienDatPhong,
+                TinhTrang = "Đã được duyệt",
+                SoTienTraTruoc = soTienTraTruoc,
+            };
+            _db.DatPhong.Add(DatPhong);
+            if (arrMaDichVu != null || arrSoLuongDichVu != null)
+            {
+
+
+                for (int i = 0; i < arrMaDichVu.Count; i++)
+                {
+                    var maDichVu = arrMaDichVu[i];
+                    var SoLuongDichVu = arrSoLuongDichVu[i];
+
+                    var DichVu = new ChiTietDichVu
+                    {
+                        MaDichVu = maDichVu,
+                        MaKhachHang = maKhachHang,
+                        SoLuong = SoLuongDichVu,
+                        MaNhanVien = MaNhanVienDatPhong,
+                        ThoiGianDichVu = DateTime.Now,
+                        TrangThai = "Hoạt động",
+                        MaDatPhong = MaDatPhong
+                    };
+                    _db.ChiTietDichVu.Add(DichVu);
+                }
+            }
+            _db.SaveChanges();
+
+
+            return Json(new { arrMaDichVu, arrSoLuongDichVu });
+        }
+        [HttpGet]
+        public IActionResult LayThanhToanMaDatPhong(string maPhong)
+        {
+            var qr_MaDatPhongTheoNgayGioHienTai = _db.DatPhong.FirstOrDefault(s => s.MaPhong == maPhong && s.NgayNhan <= DateTime.Now && s.NgayTra >= DateTime.Now);
+            var qr_MaKhachHangTheoMaDatPhong = _db.KhachHang.FirstOrDefault(s=>s.MaKhachHang == qr_MaDatPhongTheoNgayGioHienTai.MaKhachHang);
+
+            var qr_DichVuTheoMaDatPhong = _db.ChiTietDichVu
+          .Where(s => s.MaDatPhong == qr_MaDatPhongTheoNgayGioHienTai.MaDatPhong)
+          .Join(_db.DichVu, ctdv => ctdv.MaDichVu, dv => dv.MaDichVu, (ctdv, dv) => new { ctdv.MaDichVu, ctdv.SoLuong, dv.TenDichVu,dv.GiaTien })
+          .ToList(); // Include MaDichVu, SoLuong, and TenDichVu in the result
+
+            return Json(new { qr_MaDatPhongTheoNgayGioHienTai ,qr_MaKhachHangTheoMaDatPhong,qr_DichVuTheoMaDatPhong});
+
         }
     }
 }
--- QuanLyKhachSan/wwwroot/JsDatPhong/JsDatPhong.js
+++ QuanLyKhachSan/wwwroot/JsDatPhong/JsDatPhong.js
@@ -12,7 +12,6 @@
             document.getElementById('giaPhong_PopUpDatPhong').value = data.giaTheoGio;
             document.getElementById('hinhThuc_PopUpDatPhong').onchange = function () {
                 var hinhthuc = document.getElementById('hinhThuc_PopUpDatPhong').value;
-                console.log(hinhthuc);
                 if (hinhthuc == 'Ngay') {
                     document.getElementById('giaPhong_PopUpDatPhong').value = data.giaPhongTheoNgay;
 
@@ -21,6 +20,8 @@
                     document.getElementById('giaPhong_PopUpDatPhong').value = data.giaTheoGio;
 
                 }
+                TinhTongTienPhong();
+             
             }
 
         },
@@ -33,6 +34,8 @@
 
     document.getElementById('ngayNhanPhong_PopUpDatPhong').value = new Date().toISOString().slice(0, 16);
     document.getElementById('ngayTraPhong_PopUpDatPhong').value = new Date().toISOString().slice(0, 16);
+
+
 }
 
 function showForm() {
@@ -56,17 +59,65 @@ function closePopup() {
     document.getElementById("popupOverlay").style.display = "none";
 }
 
-function openPopupThanhToan() {
+function openPopupThanhToan(maPhong) {
     document.getElementById("paymentPopup").style.display = "block";
     document.getElementById("overlayThanhToan").style.display = "block";
+    document.getElementById('MaPhong_ThanhToan').innerHTML = maPhong;
+
+    // Gọi AJAX với tham số mã phòng
+    $.ajax({
+        url: "/DatPhong/LayThanhToanMaDatPhong",
+        data: { maPhong: maPhong }, // Truyền tham số mã phòng vào request
+        type: "GET", // Hoặc "POST" tùy thuộc vào cấu hình của server
+        success: function (result) {
+            console.log(result);
+            document.getElementById('maDatPhongThanhToan_PopUp').innerHTML = result.qr_MaDatPhongTheoNgayGioHienTai.maDatPhong;
+            document.getElementById('soLuongNguoiLonThanhToan_PopUp').innerHTML = result.qr_MaDatPhongTheoNgayGioHienTai.soLuongNguoiLon;
+            document.getElementById('soLuongTreEmThanhToan_PopUp').innerHTML = result.qr_MaDatPhongTheoNgayGioHienTai.soLuongTreEm;
+            document.getElementById('thoiGianNhanThanhToan_PopUp').innerHTML = result.qr_MaDatPhongTheoNgayGioHienTai.ngayNhan;
+            document.getElementById('thoiGianTraThanhToan_PopUp').innerHTML = result.qr_MaDatPhongTheoNgayGioHienTai.ngayTra;
+            document.getElementById('tenKhachHangThanhToan_PopUp').innerHTML = result.qr_MaKhachHangTheoMaDatPhong.tenKhachHang;
+            document.getElementById('soDienThoaiThanhToan_PopUp').innerHTML = result.qr_MaKhachHangTheoMaDatPhong.soDienThoai;
+            var table = document.querySelector('.table');
+            while (table.rows.length > 1) {
+                table.deleteRow(1);
+            }
+            for (var i = 0; i < result.qr_DichVuTheoMaDatPhong.length; i++) {
+                var service = result.qr_DichVuTheoMaDatPhong[i];
+
+                // Add a new row
+                var row = table.insertRow(-1);
+
+                // Add cells with relevant data
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+                var cell5 = row.insertCell(4);
+                var cell6 = row.insertCell(5);
+
+                // Populate cells with data
+                cell1.innerHTML = i + 1; // Row number
+                cell2.innerHTML = service.maDichVu; // Service code
+                cell3.innerHTML = service.tenDichVu; // Replace with actual service name
+                cell4.innerHTML = service.soLuong; // Replace with actual quantity
+                cell5.innerHTML = service.giaTien;
+                cell6.innerHTML = "Total Amount"; 
+                console.log(service)
+            }
+        },
+        error: function (xhr, status, error) {
+            // Xử lý lỗi nếu có
+            console.log(error);
+        }
+    });
 }
 
 function closePopupThanhToan() {
     document.getElementById("paymentPopup").style.display = "none";
     document.getElementById("overlayThanhToan").style.display = "none";
 }
-
-var arrDichVuDaChon = [];
+var arrDichVuDaChon=[];
 var counter = -1;
 var soLuongdichVu;
 
@@ -74,7 +125,7 @@ document.getElementById('AddThemDichVu').addEventListener('click', ThemDichVu);
 function ThemDichVu() {
     counter++;
 
-    var url = '/DatPhong/LayDanhSachDichVu';
+    var url = '/TrangChuKhachHang/LayDanhSachDichVu';
 
     $.ajax({
         url: url,
@@ -112,9 +163,7 @@ function ThemDichVu() {
             arrDichVuDaChon[counter] = dichVuDaChonTheoId;
 
             $('#themdichvuselect' + counter).focus(function () {
-                // Lặp qua tất cả các option
                 $(this).find('.optionChonDichVu').each(function () {
-                    // Nếu giá trị option nằm trong mảng arrDichVuDaChon, ẩn option đó
                     if (arrDichVuDaChon.includes($(this).val())) {
                         $(this).hide();
                     } else {
@@ -126,7 +175,11 @@ function ThemDichVu() {
                 document.getElementById('AddThemDichVu').style.display = 'none';
 
             }
-           
+            var dichVuDaChon = document.getElementById('themdichvuselect' + counter).value;
+            var soLuongDichVu = document.getElementById('soLuongDichVu' + counter).value;
+            var nodeServices = `<br id="breakDichVu${counter}"/><span id="tongTenDichVuDaDat${counter}">Tên dịch vụ: ${dichVuDaChon} | </span><span id="tongSoLuongDichVuDaDat${counter}">Số lượng: ${soLuongDichVu}</span>`;
+            $('#TongDichVuDaDat').append(nodeServices);
+            TinhTienDichVu();
         },
         error: function (error) {
             console.error('Error fetching data:', error);
@@ -136,6 +189,32 @@ function ThemDichVu() {
 
 
 }
+function TinhTienDichVu() {
+    var ThanhTienMaDichVu = [];
+    var arrSoLuongDichVu = [];
+    $('span[id^="tongTenDichVuDaDat"]').each(function () {
+        ThanhTienMaDichVu.push($(this).text().split(": ")[1].split(" | ")[0]);
+    });
+    $('span[id^="tongSoLuongDichVuDaDat"]').each(function () {
+        arrSoLuongDichVu.push($(this).text().split(": ")[1]);
+    });
+    $.ajax({
+        url: '/TrangChuKhachHang/TinhTienDichVu',
+        type: 'POST',
+        data: { 'arrMaDichVu': ThanhTienMaDichVu, 'arrSoLuongDichVu': arrSoLuongDichVu },
+        traditional: true,
+        success: function (result) {
+
+            document.getElementById('TongTienDichVuDaDat').innerText = result;
+            TongTienDatPhong();
+            
+        },
+        error: function (xhr, status, error) {
+        }
+    });
+   
+}
+
 function DichVuDaChon(id) {
 
     var dichVuDaChonTheoId = document.getElementById('themdichvuselect' + id).value;
@@ -149,23 +228,107 @@ function DichVuDaChon(id) {
 
         var tongTextTenDichVuDaDat = "tongTenDichVuDaDat" + id;
         var tongTextSoLuongDichVuDaDat = document.getElementById("tongSoLuongDichVuDaDat" + id);
-        //document.getElementById(tongTextTenDichVuDaDat).innerText = `Tên dịch vụ: ${dichVuDaChon} | `;
-        //tongTextSoLuongDichVuDaDat.innerText = `Số lượng: ${soLuongDichVu}`;
+        document.getElementById(tongTextTenDichVuDaDat).innerText = `Tên dịch vụ: ${dichVuDaChon} | `;
+        tongTextSoLuongDichVuDaDat.innerText = `Số lượng: ${soLuongDichVu}`;
         arrDichVuDaChon[id] = dichVuDaChonTheoId;
+        TinhTienDichVu();
     }
 }
 
+function ThayDoiSoLuongDichVu(id) {
+    var SoLuongDichVuDaThayDoi = document.getElementById('soLuongDichVu' + id).value;
+    var tongTextSoLuongDichVuDaDat = document.getElementById("tongSoLuongDichVuDaDat" + id);
+    tongTextSoLuongDichVuDaDat.innerText = `Số lượng: ${SoLuongDichVuDaThayDoi}`;
+    TinhTienDichVu();
+}
 function XoaSelectDichVu(idSelect) {
     var idTongTheDichVu = 'id_DichVu' + idSelect;
     document.getElementById(idTongTheDichVu).remove();
     arrDichVuDaChon[idSelect] = "";
     document.getElementById('AddThemDichVu').style.display = 'block';
-    //xóa text dịch vụ đã chọn
-    //var tongTextTenDichVuDaDat = "tongTenDichVuDaDat" + idSelect;
-    //var tongTextSoLuongDaDat = "tongSoLuongDichVuDaDat" + idSelect;
-    //var breakTongDichVu = "breakDichVu" + idSelect;
-    //document.getElementById(tongTextSoLuongDaDat).remove();
-    //document.getElementById(tongTextTenDichVuDaDat).remove();
-    //document.getElementById(breakTongDichVu).remove();
+    var tongTextTenDichVuDaDat = "tongTenDichVuDaDat" + idSelect;
+    var tongTextSoLuongDaDat = "tongSoLuongDichVuDaDat" + idSelect;
+    var breakTongDichVu = "breakDichVu" + idSelect;
+    document.getElementById(tongTextSoLuongDaDat).remove();
+    document.getElementById(tongTextTenDichVuDaDat).remove();
+    document.getElementById(breakTongDichVu).remove();
+    TinhTienDichVu();
+}
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('hinhThuc_PopUpDatPhong').addEventListener('change', updateTotal);
+    document.getElementById('ngayNhanPhong_PopUpDatPhong').addEventListener('change', updateTotal);
+    document.getElementById('ngayTraPhong_PopUpDatPhong').addEventListener('change', updateTotal);
+    function updateTotal() {
+        TinhTongTienPhong();
+        
+    }
+
+});
+function TinhTongTienPhong() {
+    var hinhThuc = document.getElementById('hinhThuc_PopUpDatPhong').value;
+    var ngayNhanPhong = new Date(document.getElementById('ngayNhanPhong_PopUpDatPhong').value);
+    var ngayTraPhong = new Date(document.getElementById('ngayTraPhong_PopUpDatPhong').value);
+    var duration = 0;
+    if (hinhThuc === 'Ngay') {
+        duration = (ngayTraPhong - ngayNhanPhong) / (1000 * 60 * 60 * 24);
+    } else {
+        duration = (ngayTraPhong - ngayNhanPhong) / (1000 * 60 * 60);
+    }
+    var giaPhong = parseFloat(document.getElementById('giaPhong_PopUpDatPhong').value);
+    var totalAmount = giaPhong * duration;
+    document.getElementById('TongTienPhong').innerHTML = totalAmount.toFixed(2);
+    TongTienDatPhong();
+}
+function TongTienDatPhong() {
+    var tongTienPhong = parseFloat(document.getElementById('TongTienPhong').innerHTML);
+    var tongTienDichVu = parseFloat(document.getElementById('TongTienDichVuDaDat').innerHTML);
+    var TongTienDatPhong = tongTienPhong + tongTienDichVu;
+    document.getElementById('TongTien').innerHTML = TongTienDatPhong;
 }
 
+document.getElementById('NhanPhong_PopUp').addEventListener('click', DatPhong);
+function DatPhong() {
+    var ThanhTienMaDichVu = [];
+    var arrSoLuongDichVu = [];
+    $('span[id^="tongTenDichVuDaDat"]').each(function () {
+        ThanhTienMaDichVu.push($(this).text().split(": ")[1].split(" | ")[0]);
+    });
+    $('span[id^="tongSoLuongDichVuDaDat"]').each(function () {
+        arrSoLuongDichVu.push($(this).text().split(": ")[1]);
+    });
+    var maKhachHangDatPhong = document.getElementById('MaKhachHangDatPhong').value;
+    var maKhachHang = maKhachHangDatPhong.split('|')[0].trim();
+    var hinhThuc = document.getElementById('hinhThuc_PopUpDatPhong').value;
+    var ngayNhan = document.getElementById('ngayNhanPhong_PopUpDatPhong').value;
+    var ngayTra = document.getElementById('ngayTraPhong_PopUpDatPhong').value;
+    var tongNguoiLon = document.getElementById('tongNguoiLon').value;
+    var tongTreEm = document.getElementById('tongTreEm').value;
+    var tongTienPhong = document.getElementById('TongTien').innerText;
+    var khachTraTruoc = document.getElementById('khachTraTruoc').value;
+    var maPhong = document.getElementById('MaPhong_PopUp_DatPhong').innerText;
+    console.log(tongTienPhong, khachTraTruoc);
+    // Gửi AJAX request
+    $.ajax({
+        url: '/DatPhong/DatPhongNhanh',
+        type: 'POST',
+        data: {
+            'arrMaDichVu': ThanhTienMaDichVu,
+            'arrSoLuongDichVu': arrSoLuongDichVu,
+            'maKhachHang': maKhachHang,
+            'hinhThuc': hinhThuc,
+            'ngayNhan': ngayNhan,
+            'ngayTra': ngayTra,
+            'tongNguoiLon': tongNguoiLon,
+            'tongTreEm': tongTreEm,
+            'tongCong': tongTienPhong,
+            'soTienTraTruoc': khachTraTruoc,
+            'maPhong': maPhong
+        },
+        traditional: true,
+        success: function (result) {
+            console.log(result)
+        },
+        error: function (xhr, status, error) {
+        }
+    });
+}
