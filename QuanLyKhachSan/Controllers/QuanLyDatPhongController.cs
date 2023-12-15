470e8d484a486131f7788556c582bc12f4781661
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-12-15 12:42:24 +0700

    ~quanlydatphong

---
+++ QuanLyKhachSan/Controllers/QuanLyDatPhongController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Finance.Implementations;
+using QuanLyKhachSan.Models;
+
+namespace QuanLyKhachSan.Controllers
+{
+    public class QuanLyDatPhongController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public QuanLyDatPhongController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var qr_DatPhong = _db.DatPhong.ToList();
+            return View(qr_DatPhong);
+        }
+    }
+}
