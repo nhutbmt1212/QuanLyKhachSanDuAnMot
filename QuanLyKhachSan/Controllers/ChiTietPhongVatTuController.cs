517748815e08f80e7b56999e444206efb4325be5
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-12-18 10:47:04 +0700

    xong chitietphongvattu

--- QuanLyKhachSan/Controllers/ChiTietPhongVatTuController.cs
+++ QuanLyKhachSan/Controllers/ChiTietPhongVatTuController.cs
@@ -15,5 +15,89 @@ namespace QuanLyKhachSan.Controllers
             var qr_PhongVatTu = _db.ChiTietPhongVatTu.Where(s=>s.TinhTrang != "Đã xóa").ToList();
             return View(qr_PhongVatTu);
         }
+        [HttpPost]
+        public IActionResult ThemVatTuVaoPhong(string MaPhong, string MaVatTu, string SoLuong)
+        {
+            var existingChiTietPhongVatTu = _db.ChiTietPhongVatTu
+                .FirstOrDefault(ct => ct.MaPhong == MaPhong && ct.MaVatTu == MaVatTu);
+
+            if (existingChiTietPhongVatTu != null)
+            {
+                existingChiTietPhongVatTu.SoLuong += int.Parse(SoLuong);
+            }
+            else
+            {
+                var newChiTietPhongVatTu = new ChiTietPhongVatTu
+                {
+                    MaPhong = MaPhong,
+                    MaVatTu = MaVatTu,
+                    SoLuong = int.Parse(SoLuong),
+                    TinhTrang ="Đang hoạt động",
+                };
+
+                _db.ChiTietPhongVatTu.Add(newChiTietPhongVatTu);
+            }
+
+            _db.SaveChanges();
+
+            // Chuyển hướng về trang Index của ChiTietPhongVatTu
+            return RedirectToAction("Index", "ChiTietPhongVatTu");
+        }
+        [HttpPost]
+        public IActionResult SuaPhongVatTu(string MaPhong, string MaVatTu, string SoLuong, string TinhTrang)
+        {
+            try
+            {
+                var chiTietPhongVatTu = _db.ChiTietPhongVatTu
+                    .FirstOrDefault(ct => ct.MaPhong == MaPhong && ct.MaVatTu == MaVatTu);
+
+                if (chiTietPhongVatTu != null)
+                {
+                    chiTietPhongVatTu.SoLuong = int.Parse(SoLuong);
+                    chiTietPhongVatTu.TinhTrang = TinhTrang;
+
+                    _db.SaveChanges();
+
+                    return Json(new { success = true, message = "Cập nhật thành công." });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy chi tiết phòng vật tư." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Đã xảy ra lỗi. Vui lòng thử lại." });
+            }
+        }
+        [HttpPost]
+        public IActionResult XoaPhongVatTu(string MaPhong, string MaVatTu)
+        {
+            try
+            {
+                var chiTietPhongVatTu = _db.ChiTietPhongVatTu
+                    .FirstOrDefault(ct => ct.MaPhong == MaPhong && ct.MaVatTu == MaVatTu);
+
+                if (chiTietPhongVatTu != null)
+                {
+                    // Thay đổi trạng thái thành đã xóa
+                    chiTietPhongVatTu.TinhTrang = "Đã xóa";
+
+                    // Lưu thay đổi vào cơ sở dữ liệu
+                    _db.SaveChanges();
+
+                    return Json(new { success = true, message = "Đã xóa thành công." });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy chi tiết phòng vật tư." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Đã xảy ra lỗi. Vui lòng thử lại." });
+            }
+        }
+
     }
 }
---
+++ QuanLyKhachSan/wwwroot/js/JsPhongVatTu.js
@@ -0,0 +1,85 @@
+﻿function ThemVatTu() {
+    var maPhong = $('#inputFieldChonPhong').val();
+    var maVatTu = $('#inputFieldChonVatTu').val();
+    var soLuong = $('#inputFieldSoLuong').val();
+    $.ajax({
+        type: 'POST',
+        url: '/ChiTietPhongVatTu/ThemVatTuVaoPhong',
+        data: {
+            'MaPhong': maPhong,
+            'MaVatTu': maVatTu,
+            'SoLuong': soLuong
+        },
+        success: function () {
+        },
+        error: function () {
+        }
+    });
+}
+var btn_them = document.getElementById('btn_themPhong');
+function SuaVatTu(btn, maPhong, maVatTu, soLuong, tinhTrang) {
+    document.getElementById("myForm").style.display = "none";
+    btn_them.style.display = 'block';
+    document.getElementById("myFormEdit").style.display = "block";
+    $('#inputEditMaPhong').val(maPhong);
+    $('#inputEditMaVatTu').val(maVatTu);
+    $('#inputEditSoLuong').val(soLuong);
+    $('#inputEditTinhTrang').val(tinhTrang);
+}
+
+function SuaVatTuPhong() {
+    var maPhong=  $('#inputEditMaPhong').val();
+    var maVatTu=  $('#inputEditMaVatTu').val();
+    var soLuong=  $('#inputEditSoLuong').val();
+    var tinhTrang = $('#inputEditTinhTrang').val();
+    $.ajax({
+        type: "POST",
+        url: "/ChiTietPhongVatTu/SuaPhongVatTu",
+        data: {
+            'MaPhong': maPhong,
+            'MaVatTu': maVatTu,
+            'SoLuong': soLuong,
+            'TinhTrang': tinhTrang
+        },
+         success: function (data) {
+            // Xử lý phản hồi thành công từ server
+            // data có thể chứa thông tin phản hồi từ server (nếu có)
+
+            // Ví dụ: Hiển thị thông báo thành công
+            alert('Sửa thành công');
+
+            // Cập nhật giao diện hoặc thực hiện các bước khác (nếu cần)
+        },
+        error: function (error) {
+            // Xử lý lỗi khi gửi yêu cầu đến server
+            // error chứa thông tin lỗi từ server (nếu có)
+
+            // Ví dụ: Hiển thị thông báo lỗi
+            alert('Đã xảy ra lỗi. Vui lòng thử lại.');
+
+            // Thực hiện các bước khác tùy thuộc vào nhu cầu
+        }
+    });
+
+}
+
+function XoaVatTuPhong(MaVatTu, MaPhong) {
+    
+    $.ajax({
+        type: "POST",
+        url: "/ChiTietPhongVatTu/XoaPhongVatTu",
+        data: {
+            'MaPhong': MaPhong,
+            'MaVatTu': MaVatTu
+        },
+        success: function (data) {
+          
+            alert('Xóa thành công');
+
+        },
+        error: function (error) {
+        
+            alert('Đã xảy ra lỗi. Vui lòng thử lại.');
+        }
+    });
+}
